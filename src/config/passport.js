ce1a6911f5d0f3dbe662ee7d69a890d507b56d22
dmclink <135415819+dmclink@users.noreply.github.com>
2026-03-21 21:53:07 +0800

    feat: implement boilerplate jwt sign and serve

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,10 +2,31 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const passport = require('passport');
+const { prisma } = require('../../lib/prisma.js');
 
-const opts = {};
+const opts = {
+	secretOrKey: process.env.PERSONAL_BLOG_JWT_SECRET_KEY,
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	issuer: 'https://www.github.com/dmclink/personal-blog-server',
+	audience: 'personal-blog-user',
+	jsonWebTokenOptions: {},
+};
 
-const strategy = new JwtStrategy(opts);
+const verifyCallback = (jwt_payload, done) => {
+	//FIXME: delete this
+	console.log(jwt_payload);
+	try {
+		const users = prisma.user.findMany();
+		console.log(users);
+	} catch (err) {
+		console.error(err);
+	}
+	console.log('called the verify callback');
+	return done(err, false);
+	//TODO: implement verify callback by querying prisma for the user id
+};
+
+const strategy = new JwtStrategy(opts, verifyCallback);
 
 passport.use(strategy);
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,32 @@
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 
 const passport = require('../config/passport.js');
 
 router.post('/login', (req, res) => {
-	res.send('you logged in, nice\n');
+	//FIXME: delete this and put a real one
+	const payload = {
+		id: 1,
+		email: 'dmclink@gmail.com',
+		iss: 'https://www.github.com/dmclink/personal-blog-server',
+		aud: 'personal-blog-user',
+	};
+
+	//FIXME: change the expires time
+	const token = jwt.sign(payload, process.env.PERSONAL_BLOG_JWT_SECRET_KEY, {
+		expiresIn: '30s',
+	});
+
+	res.json({
+		success: true,
+		token: 'Bearer ' + token,
+	});
+});
+
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+	console.log(req);
+	res.send("cool, you're authenticated, nice\n");
 	//TODO:
 });
 
