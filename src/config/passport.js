602dcd54832d0d89a4b21d8c73f57a009abc407b
dmclink <135415819+dmclink@users.noreply.github.com>
2026-03-22 14:35:19 +0800

    feat: implement registration and authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const passport = require('passport');
-const { prisma } = require('../../lib/prisma.js');
+const { prisma } = require('../../prisma_lib/prisma.js');
 
 const opts = {
 	secretOrKey: process.env.PERSONAL_BLOG_JWT_SECRET_KEY,
@@ -12,18 +12,18 @@ const opts = {
 	jsonWebTokenOptions: {},
 };
 
-const verifyCallback = (jwt_payload, done) => {
-	//FIXME: delete this
-	console.log(jwt_payload);
+const verifyCallback = async (jwt_payload, done) => {
+	console.log('JWT_PAYLOAD:', jwt_payload);
 	try {
-		const users = prisma.user.findMany();
-		console.log(users);
+		const user = await prisma.user.findUnique({ where: { id: jwt_payload.sub } });
+		if (!user) {
+			return done(null, false);
+		}
+		return done(null, user);
 	} catch (err) {
 		console.error(err);
+		return done(err, false);
 	}
-	console.log('called the verify callback');
-	return done(err, false);
-	//TODO: implement verify callback by querying prisma for the user id
 };
 
 const strategy = new JwtStrategy(opts, verifyCallback);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,19 +4,58 @@ const router = express.Router();
 
 const passport = require('../config/passport.js');
 
-router.post('/login', (req, res) => {
-	//FIXME: delete this and put a real one
-	const payload = {
-		id: 1,
-		email: 'dmclink@gmail.com',
-		iss: 'https://www.github.com/dmclink/personal-blog-server',
-		aud: 'personal-blog-user',
-	};
+const { addUser, emailExists, usernameExists, getUsers } = require('../db/queries.js');
+const { verifyPassword, buildUserAuthToken } = require('../lib/authutils.js');
 
-	//FIXME: change the expires time
-	const token = jwt.sign(payload, process.env.PERSONAL_BLOG_JWT_SECRET_KEY, {
-		expiresIn: '30s',
-	});
+router.post('/login', async (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	try {
+		const user = await db.getUserByUsername(username);
+		const token = buildUserAuthToken(user);
+
+		res.json({
+			success: true,
+			token: 'Bearer ' + token,
+		});
+	} catch (err) {
+		console.error(err);
+		res.json({ error: { message: 'incorrect username or password' } });
+	}
+});
+
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+	console.log('REQ.USER:', req.user);
+	res.send("cool, you're authenticated, nice\n");
+});
+
+router.post('/register', async (req, res) => {
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (!email || !username || !password) {
+		res.send('hey you didnt fill out all the data, try again\n');
+		return;
+	}
+	const users = await getUsers();
+	console.log('email, username, pass:', email, username, password);
+	console.log('ALL USERS:', users);
+
+	if (await usernameExists(username)) {
+		res.send('that username already exists, pick another one\n');
+		return;
+	}
+
+	if (await emailExists(username)) {
+		res.send('that email already exists, pick another one\n');
+		return;
+	}
+
+	const user = await addUser(email, username, password);
+
+	const token = buildUserAuthToken(user);
 
 	res.json({
 		success: true,
@@ -24,10 +63,6 @@ router.post('/login', (req, res) => {
 	});
 });
 
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
-	console.log(req);
-	res.send("cool, you're authenticated, nice\n");
-	//TODO:
-});
+router.get('/verify-email', passport.authenticate('jwt', { session: false }), (req, res) => {});
 
 module.exports = router;
