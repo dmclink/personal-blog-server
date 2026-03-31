cb4b17427fcb837721c39a577d8fce27419a953d
dmclink <135415819+dmclink@users.noreply.github.com>
2026-03-31 22:04:27 +0800

    fix: missing jwt secret key in .env.example

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ app.use((error, req, res, next) => {
 	});
 });
 
-const PORT = process.env.PORT || 3000;
-console.log('listening on port: ', PORT);
-app.listen(PORT, '0.0.0.0');
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+console.log('listening on port: ', port);
+app.listen(port, host);
